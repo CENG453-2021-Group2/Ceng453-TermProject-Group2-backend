90c62043f737d7cd6138ec911e9f5e94dc8302d9
Deniz Koluaçık <koluacik@disroot.org>
2022-04-18 20:19:39 +0300

    initial commit

---
+++ src/test/java/group2/monopoly/MonopolyApplicationTests.java
@@ -0,0 +1,13 @@
+package group2.monopoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopolyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
