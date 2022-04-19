1a47d63f149ca0017ee3c70c3a1624f29196f5c6
Deniz Koluaçık <koluacik@disroot.org>
2022-04-19 17:26:55 +0300

    chore(profiles): Add profiles
    - Prod: reads env variables to connect to the production db server
    - Dev: Connects to the local mysql server
    - Test: Uses H2 in memory db

--- src/test/java/group2/monopoly/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/group2/monopoly/repository/UserRepositoryIntegrationTest.java
@@ -7,13 +7,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import javax.sql.DataSource;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class UserRepositoryIntegrationTest {
