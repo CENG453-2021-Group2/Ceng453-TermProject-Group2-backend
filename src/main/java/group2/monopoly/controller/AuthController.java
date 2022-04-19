6bd5ad2adcfed0907f60a9dda9898fb5096a4ba6
Deniz Koluaçık <koluacik@disroot.org>
2022-04-19 17:27:36 +0300

    chore: Optimize imports

--- src/main/java/group2/monopoly/controller/AuthController.java
+++ src/main/java/group2/monopoly/controller/AuthController.java
@@ -7,7 +7,6 @@ import group2.monopoly.payload.LoginDto;
 import group2.monopoly.payload.SignUpDto;
 import group2.monopoly.repository.RoleRepository;
 import group2.monopoly.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
