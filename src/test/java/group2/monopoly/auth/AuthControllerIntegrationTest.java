ede9abde6f65fc47d055ade121400f1447f8fb7e
Deniz Koluaçık <koluacik@disroot.org>
2022-04-20 21:48:13 +0300

    test: (in progress) register integration tests

---
+++ src/test/java/group2/monopoly/auth/AuthControllerIntegrationTest.java
@@ -0,0 +1,138 @@
+package group2.monopoly.auth;
+
+import group2.monopoly.MonopolyApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = WebEnvironment.MOCK,
+        classes = MonopolyApplication.class
+)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class AuthControllerIntegrationTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void givenGoodRequest_whenRegister_thenStatus200() throws Exception {
+        mvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "username": "username",
+                            "password": "password123",
+                            "email": "example@example.com"
+                        }
+                        """)
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void givenBadRequestNoUsername_whenRegister_thenStatus400() throws Exception {
+        mvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "password": "password123",
+                            "email": "example@example.com"
+                        }
+                        """)
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void givenBadRequestNoEmail_whenRegister_thenStatus400() throws Exception {
+        mvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "username": "username",
+                            "password": "password123",
+                        }
+                        """)
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void givenBadRequestNoPassword_whenRegister_thenStatus400() throws Exception {
+        mvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "username": "username",
+                            "email": "example@example.com"
+                        }
+                        """)
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void givenBadRequestWeakPassword_whenRegister_thenStatus400() throws Exception {
+        mvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "username": "username",
+                            "password": "weakaf",
+                            "email": "example@example.com"
+                        }
+                        """)
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void givenBadRequestInvalidEmail_whenRegister_thenStatus400() throws Exception {
+        mvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "username": "username",
+                            "password": "nicepassword123",
+                            "email": "bogus"
+                        }
+                        """)
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void givenBadRequestInvalidUsername_whenRegister_thenStatus400() throws Exception {
+        mvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "username": "unnecessarilylongpasswordthatislikelytoberejected",
+                            "password": "nicepassword123",
+                            "email": "example@example.com"
+                        }
+                        """)
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void givenDuplicateUsername_whenRegister_thenStatus400() throws Exception {
+        mvc.perform(post("/api/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "username": "unnecessarilylongpasswordthatislikelytoberejected",
+                            "password": "nicepassword123",
+                            "email": "example@example.com"
+                        }
+                        """)
+        ).andExpect(status().isBadRequest());
+    }
+
+}
