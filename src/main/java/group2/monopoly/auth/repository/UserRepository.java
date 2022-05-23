35acd595b97c3f8cedc7d54b36732415ce22b515
Deniz Koluaçık <koluacik@disroot.org>
2022-05-23 18:42:55 +0300

    WIP - chore(auth): Remove unused UserRepository methods

--- src/main/java/group2/monopoly/auth/repository/UserRepository.java
+++ src/main/java/group2/monopoly/auth/repository/UserRepository.java
@@ -13,14 +13,11 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
     Optional<User> findByUsername(String username);
+
     Optional<User> findById(Long id);
 
-    Optional<User> findByEmailAndRoleTrue(String email);
-    Optional<User> findByUsernameAndRoleTrue(String username);
-    Optional<User> findByIdAndRoleTrue(Long id);
-
-    Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 
 }
