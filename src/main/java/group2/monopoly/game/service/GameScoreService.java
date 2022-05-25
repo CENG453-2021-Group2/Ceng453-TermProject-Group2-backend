1d7a5d421bfe506e98f57e16fe189338355e0e2b
Deniz Koluaçık <koluacik@disroot.org>
2022-05-25 22:54:29 +0300

    feat(game): implement price and score calculation services

---
+++ src/main/java/group2/monopoly/game/service/GameScoreService.java
@@ -0,0 +1,31 @@
+package group2.monopoly.game.service;
+
+import group2.monopoly.game.entity.GameTableConfiguration;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameScoreService {
+
+    private final IGameCellPrice gameCellPriceService;
+
+    public GameScoreService(IGameCellPrice gameCellPriceService) {
+        this.gameCellPriceService = gameCellPriceService;
+    }
+
+    public Integer computeScore(GameTableConfiguration table, List<Integer> properties, Integer money) {
+        Integer propertyScore = properties
+                .stream()
+                .filter(table.getPropertyIndices()::contains) // one of the 8 properties
+                .map(cellIndex -> gameCellPriceService.getPropertyPrice(table, cellIndex))
+                .reduce(0, Integer::sum);
+        Integer portScore = properties
+                .stream()
+                .filter(table.getPortIndices()::contains) // one of the 4 ports
+                .map(cellIndex -> gameCellPriceService.getPortPrice(table, cellIndex))
+                .reduce(0, Integer::sum);
+
+        return portScore + propertyScore + money;
+    }
+}
