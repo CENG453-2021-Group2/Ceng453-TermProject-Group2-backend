85c1aa9c9a684a9ac00a3dbd7f143ecffbef8e1e
Deniz Koluaçık <koluacik@disroot.org>
2022-05-26 09:02:40 +0300

    feat(game): implement scoreboard service and controller

--- src/main/java/group2/monopoly/game/repository/PlayerRepository.java
+++ src/main/java/group2/monopoly/game/repository/PlayerRepository.java
@@ -5,6 +5,7 @@ import group2.monopoly.game.entity.Game;
 import group2.monopoly.game.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -16,6 +17,6 @@ public interface PlayerRepository extends JpaRepository<Player, Long> {
     @Query("select p from player p where p.game = ?1 order by p.turnOrder")
     List<Player> findAllByGameOrderByTurnOrder(Game game);
 
-    @Query("select p from player p inner join game g where g.completionDate is not null and g.completionDate > ?1 and g.completionDate < ?2 order by p.score desc")
-    List<Game> findAllByGameNotNull(Date start, Date end);
+    @Query("select p from player p inner join game g where g.completionDate is not null and g.completionDate > :startdate and g.completionDate < :enddate order by p.score desc")
+    List<Player> findAllByGameNotNull(@Param("startdate") Date start, @Param("enddate") Date end);
 }
