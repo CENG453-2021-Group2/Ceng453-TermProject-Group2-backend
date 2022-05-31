607b1068404da16e75ed12a9907e2ec72c10e20a
Deniz Koluaçık <koluacik@disroot.org>
2022-05-31 23:28:55 +0300

    feat(game): implement faulty move exception handler

--- src/main/java/group2/monopoly/handlers/GameExceptionHandler.java
+++ src/main/java/group2/monopoly/handlers/GameExceptionHandler.java
@@ -3,6 +3,7 @@ package group2.monopoly.handlers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import group2.monopoly.game.exception.GameFaultyMoveException;
 import group2.monopoly.game.exception.GameOverException;
 import group2.monopoly.mapper.ObjectMapperSingleton;
 import lombok.extern.slf4j.Slf4j;
@@ -20,13 +21,23 @@ public class GameExceptionHandler {
 
     @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler({GameOverException.class})
-    public ResponseEntity<JsonNode> handleBasicAuthException(GameOverException exception,
+    public ResponseEntity<JsonNode> handleGameOverException(GameOverException exception,
                                                              WebRequest webRequest) {
-        log.info("game over");
+        log.info("Game over.");
         ObjectNode response = mapper.createObjectNode()
                 .put("message", "game over")
                 .<ObjectNode>set("game", mapper.valueToTree(exception.getGame()))
                 .set("loser", mapper.valueToTree(exception.getPlayer()));
         return ResponseEntity.ok().body(response);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({GameFaultyMoveException.class})
+    public ResponseEntity<JsonNode> handleGameFaultyMoveException(GameFaultyMoveException exception,
+                                                             WebRequest webRequest) {
+        log.info("Invalid move");
+        ObjectNode response = mapper.createObjectNode()
+                .put("message", exception.getMessage());
+        return ResponseEntity.badRequest().body(response);
     }
 }
